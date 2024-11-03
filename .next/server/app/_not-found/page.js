7595b353f595dc472c07d85def48bb5bbe6fab27
raymondchiu10 <raymondchiu10@gmail.com>
2024-11-03 01:33:12 -0700

    added reloading and suspense

--- .next/server/app/_not-found/page.js
+++ .next/server/app/_not-found/page.js
@@ -1,9 +1,9 @@
 const CHUNK_PUBLIC_PATH = "server/app/_not-found/page.js";
 const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/69fde_next_dist_61fe3d._.js");
+runtime.loadChunk("server/chunks/ssr/195f1_next_dist_1e2c0c._.js");
 runtime.loadChunk("server/chunks/ssr/[root of the server]__adba9d._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__2afb39._.js");
+runtime.loadChunk("server/chunks/ssr/[root of the server]__f935d9._.js");
 runtime.loadChunk("server/chunks/ssr/[root of the server]__b94e07._.css");
-runtime.loadChunk("server/chunks/ssr/_00f9ac._.js");
+runtime.loadChunk("server/chunks/ssr/_28bd22._.js");
 runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/_not-found/page/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.0.1_react-dom@19.0.0-rc-cd22717c-20241013_react@19.0.0-rc-cd22717c-20241013/node_modules/next/dist/esm/build/templates/app-page.js?page=/_not-found/page { MODULE_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", MODULE_1 => \"[project]/node_modules/.pnpm/next@15.0.1_react-dom@19.0.0-rc-cd22717c-20241013_react@19.0.0-rc-cd22717c-20241013/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.0.2_react-dom@19.0.0-rc-cd22717c-20241013_react@19.0.0-rc-cd22717c-20241013/node_modules/next/dist/esm/build/templates/app-page.js?page=/_not-found/page { MODULE_0 => \"[project]/app/layout.tsx [app-rsc] (ecmascript, Next.js server component)\", MODULE_1 => \"[project]/node_modules/.pnpm/next@15.0.2_react-dom@19.0.0-rc-cd22717c-20241013_react@19.0.0-rc-cd22717c-20241013/node_modules/next/dist/client/components/not-found-error.js [app-rsc] (ecmascript, Next.js server component)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;

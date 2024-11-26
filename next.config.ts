4c7f5dfdcf0b05d12e7b2f97e821d383038ce0b9
raymondchiu10 <raymondchiu10@gmail.com>
2024-11-26 15:51:29 -0800

    Revert "Merge pull request #12 from raymondchiu10/chapter-10"
    This reverts commit 41ea2a434be24e3768166870d8cdf2123dddd5c9, reversing
    changes made to a30296f78a469fe78c370697929877190f7d030d.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: 'incremental',
-  },  
+  /* config options here */
 };
 
 export default nextConfig;
